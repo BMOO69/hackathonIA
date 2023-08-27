3ac419faea1fa81fe75380b4193dc0d261adcf68
Miguel Bernal <mb123mb1@hotmail.com>
2023-08-26 22:54:40 -0400

    funciona la obtencion de datos de la pagina web

--- chatbot/chatbotApp/urls.py
+++ chatbot/chatbotApp/urls.py
@@ -7,6 +7,11 @@ from chatbotApp import views
 urlpatterns =[
 
     path('',views.saludo, name= 'chat'),
+    path('chat/',views.chat_view, name='chat2'),
+    path('refresh_chat/', views.refresh_chat_view, name='refresh_chat'),
+    path('enviar/', views.send_message, name='enviar'),
+
+    path('buscar/', views.buscar),
 
 ]
 
